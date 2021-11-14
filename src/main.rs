a67033f4e932b23b541317d78f471603a86b7931
lucasbastida <48137388+lucasbastida@users.noreply.github.com>
2021-11-14 16:28:14 -0300

    CLI search argument created

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use serde::Deserialize;
 use std::collections::HashMap;
 
+use clap::{App, Arg};
+
+
 #[derive(Debug, Deserialize)]
 struct Coin {
     name: String,
@@ -21,16 +24,36 @@ impl Coin {
 #[derive(Debug, Deserialize)]
 struct Market {
     current_price: HashMap<String, f32>,
-    sparkline_7d: Sparkline
+    sparkline_7d: Sparkline,
 }
 
 #[derive(Debug, Deserialize)]
 struct Sparkline {
-    price: Vec<f32>
+    price: Vec<f32>,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let matches = App::new("Crypto Price Checker")
+        .version("1.0")
+        .author("Lucas B. ")
+        .about("Checks price of your portfolio and other crypto from coingecko.com")
+        .arg(
+            Arg::with_name("search")
+                .help("Crypto you want to view")
+                .takes_value(true)
+                .short("s")
+                .long("search")
+                .multiple(true),
+        )
+        .get_matches();
+
+    let crypto_names = matches.values_of("search").unwrap();
+
+    for elem in crypto_names {
+        println!("{}", elem);
+    }
+
     let resp = reqwest::get("https://api.coingecko.com/api/v3/coins/bitcoin?tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=true")
         .await?
         .json::<Coin>()
